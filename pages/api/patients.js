845ceb0cba3f0c05c8d4487e806d9240dc7b1dda
Adam Yargazi <mr.ask@list.ru>
2022-09-21 14:25:09 +0300

    res Put behavior

--- pages/api/patients.js
+++ pages/api/patients.js
@@ -33,8 +33,8 @@ export const edit = async (req, res) => {
   const updatedUser = await Users.findByIdAndUpdate(userId, data, {
     returnOriginal: false,
   });
+  res.status(200).send({ ok: true, updatedUser, message: "User deleted" });
 
-  res.status(200).send({ updatedUser });
   if (!updatedUser) {
     res.status(404).send({ message: " User not found" });
   } else {
